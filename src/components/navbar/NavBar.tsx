71ad8082483b540f4d8625f841a1c10175a35fde
Tuan Nguyen Manh <tuannm67@fpt.com>
2023-12-01 21:37:28 +0700

    push code

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Logo from "../../assets/logo.png";
+import SearchIcon from "../../assets/search-icon.png";
+import './navbar.css';
+import { IoIosSearch } from "react-icons/io";
+
+
+const NavBar = () => {
+  return (
+    <div className='navbar-wrapper'>
+      <div className='navbar-wrapper-top'>
+        <div className='navbar-logo'>
+          <img src={Logo} className='navbar-img-logo'/>
+          <a href="">Chính sách quyền riêng tư</a>
+          <button>EN</button>
+          <button>VN</button>
+        </div>
+        <div className='navbar-search'>
+          <input type="text" className='navbar-search-input' placeholder="Từ khóa tìm kiếm / OnlineCode từ công cụ chọn cấu hình xe"/>
+          <div className='navbar-search-contain'>
+            <img src={SearchIcon} className='navbar-search-image' />
+          </div>
+        </div>
+      </div>
+      <div className='navbar-wrapper-navigation'></div>
+      
+    </div>
+  )
+}
+
+export default NavBar
